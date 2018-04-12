b07aa959853defc4695f43d00966c160a459af1e
Felipe Abreu <coisa.cs@gmail.com>
2018-04-12 00:56:47 -0300

    Fix middlewares

--- config/middlewares.php
+++ config/middlewares.php
@@ -10,12 +10,5 @@ use Slim\App;
 return function (App $app) {
     $container = $app->getContainer();
 
-    /** @var Zeuxisoo\Whoops\Provider\Slim\WhoopsErrorHandler $errorHandler */
-    $errorHandler = $container->get(Zeuxisoo\Whoops\Provider\Slim\WhoopsErrorHandler::class);
-
-    /** @var Slim\HttpCache\Cache $httpCache */
-    $httpCache = $container->get(Slim\HttpCache\Cache::class);
-
-    $app->add($errorHandler);
-    $app->add($httpCache);
+    $app->add($container->get(Slim\HttpCache\Cache::class));
 };
